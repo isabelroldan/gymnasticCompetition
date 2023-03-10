7ff04fb66321737a40ed69b37db0da294658e00e
Jose Javier Galan <116640823+JoseJavier1801@users.noreply.github.com>
2023-03-10 08:47:34 +0100

    0.1

---
+++ src/main/java/iesFranciscodelosRios/model/Person.java
@@ -0,0 +1,77 @@
+package iesFranciscodelosRios.model;
+
+public class Person {
+    private String DNI;
+    private String Name;
+    private int Phone;
+    private String Mail;
+
+    public Person(){
+        this(" "," ",0," ");
+    }
+    public Person(String DNI, String name, int phone, String mail) {
+        this.DNI = DNI;
+        this.Name = name;
+        this.Phone = phone;
+        this.Mail = mail;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(int phone) {
+        Phone = phone;
+    }
+
+    public String getMail() {
+        return Mail;
+    }
+
+    public void setMail(String mail) {
+        Mail = mail;
+    }
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (DNI == null) {
+            if (other.DNI != null)
+                return false;
+        } else if (!DNI.equals(other.DNI))
+            return false;
+        if (Phone != other.Phone)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "DNI='" + DNI + '\'' +
+                ", Name='" + Name + '\'' +
+                ", Phone=" + Phone +
+                ", Mail='" + Mail + '\'' +
+                '}';
+    }
+}
