247e98211f5c7db06cec5bcecd8b474eb1cbc922
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-12 12:49:24 +0100

    0.2 primer sprint finalizado

--- src/main/java/iesFranciscodelosRios/Test/Trial/TestTrial.java
+++ src/main/java/iesFranciscodelosRios/Test/Trial/TestTrial.java
@@ -3,11 +3,8 @@ package iesFranciscodelosRios.Test.Trial;
 import iesFranciscodelosRios.Enum.Category;
 import iesFranciscodelosRios.Enum.Kit;
 import iesFranciscodelosRios.Enum.Type;
-import iesFranciscodelosRios.Utils.Read;
 import iesFranciscodelosRios.model.Participation;
 import iesFranciscodelosRios.model.Trial;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -20,17 +17,28 @@ public class TestTrial {
         iesFranciscodelosRios.Utils.Logger.CreateFile(logger,"LogTrial");
         logger.setLevel(Level.ALL);
         testAddParticipant();
+        System.out.println("-----------WINNER------------");
         testGetWinner();
-        testScore();
     }
     public static void testAddParticipant(){
         try{
             HashMap<Integer, Participation> a=new HashMap<>();
-            a.put(1,new Participation(0));
-            a.put(2,new Participation(1));
-            a.put(3,new Participation(2));
+            a.put(1,new Participation(0,""));
+            a.put(2,new Participation(1,""));
+            a.put(3,new Participation(2,""));
             t.setParticipations(a);
-            if(t.addParticipant(new Participation(4))){
+            for (Map.Entry<Integer,Participation> p:t.getParticipations().entrySet()){
+                Integer key= p.getKey();
+                if(t.score(key,(int)(Math.random()*10))){
+                    logger.info("OK se establecio puntuacion correctamente");
+                }else{
+                    logger.warning("No se pudo establecer puntuacion");
+                }
+            }
+            for (Participation<Object> p:t.getParticipations().values()){
+                System.out.println(p);
+            }
+            if(t.addParticipant(new Participation(4,""))){
                 logger.info("OK TestAddParticipant agrego un participante al hashMap");
             }else{
                 logger.info("OK. TestAddParticipant no puedo insertar participante en el hashMap");
@@ -43,10 +51,6 @@ public class TestTrial {
     public static void testGetWinner(){
         try{
             HashMap<Integer,Participation> aux=t.getParticipations();
-            for (Map.Entry<Integer,Participation> e:aux.entrySet()){
-                e.getValue().setPoints((int)(Math.random()*10));
-                aux.put(e.getKey(),e.getValue());
-            }
             for (Participation p:t.getWinner()){
                 System.out.println(p);
             }
@@ -56,13 +60,5 @@ public class TestTrial {
             logger.log(Level.SEVERE,"ERROR");
         }
     }
-    public static void testScore(){
-        int dorsal=Read.readInt("Introduce el dorsal del usuario");
-        if(t.score(dorsal,Read.readInt("introduce puntuacion"))){
-            System.out.println(t.searchParticipant(dorsal));
-            logger.info("OK se establecio puntuacion correctamente");
-        }else{
-            logger.warning("No se pudo establecer puntuacion");
-        }
-    }
 }
+
