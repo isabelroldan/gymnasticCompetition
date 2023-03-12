8a5378af8764c1b38ac0cb220410514e9629baa4
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-12 14:15:59 +0100

    0.1 Primer sprint finalizado

--- src/main/java/iesFranciscodelosRios/Controller/ControllerTrial.java
+++ src/main/java/iesFranciscodelosRios/Controller/ControllerTrial.java
@@ -2,58 +2,69 @@ package iesFranciscodelosRios.Controller;
 import iesFranciscodelosRios.Utils.Read;
 import iesFranciscodelosRios.interfaces.*;
 import iesFranciscodelosRios.GUI.*;
-public class ControllerTrial implements iController{
-    iGUI GUI= new Gui();
-    @Override
-    public void main() {
-        boolean end=false;
+import iesFranciscodelosRios.model.*;
+
+public class ControllerTrial {
+    private final iGUI GUI = new Gui();
+
+    public void main(Trial t,Competition c) {
+        boolean end = false;
         do {
-            GUI.crudTrials();
-            switch (Read.readInt("Enter a valid option")){
+            GUI.trial();
+            switch (Read.readInt("Enter a valid option")) {
                 case 0:
+                    end=true;
                     break;
                 case 1:
-                    add();
+                    addParticipant(t, c);
                     break;
                 case 2:
-                    modify();
+                    showAll(t);
                     break;
                 case 3:
-                    show();
+                    score(t);
                     break;
                 case 4:
-                    delete();
+                    showWinner(t);
                     break;
                 case 5:
 
 
-
             }
-        }while (!end);
+        } while (!end);
+    }
+    private void addParticipant(Trial t, Competition c){
+        int key=Read.readInt("Enter a bib number");
+        if(c.showParticipation(key)!=null){
+            if(t.addParticipant(c.showParticipation(key))){
+                System.out.println("The participant with bib number "+key+" was added");
+            }else{
+                System.out.println("The participant with number "+key+" already participates in the trial");
+            }
+        }else{
+            System.out.println("the participant with the number "+key+" does not exist");
+        }
+    }
+    private void showAll(Trial t){
+        if(t.getParticipations()!=null){
+            for (Participation p:t.getParticipations().values()){
+                System.out.println(p);
+            }
+        }else{
+            System.out.println("Trial "+t.name+" has no participants added");
+        }
+    }
+    private void score(Trial t){
+        System.out.println(t.score(Read.readInt("Enter the participant's bib number"),Read.readInt("point it from 0/10")));
+    }
+    private void showWinner(Trial t){
+        if(t.getWinner()!=null){
+            for (Participation p:t.getWinner()){
+                System.out.println(p);
+            }
+        }else{
+            System.out.println("There are no participants in the trial "+t.name);
+        }
     }
 
-    @Override
-    public void add() {
-
-    }
-
-    @Override
-    public void show() {
-
-    }
-
-    @Override
-    public void delete() {
-
-    }
-
-    @Override
-    public void modify() {
-
-    }
-
-    @Override
-    public void showAll() {
-
-    }
 }
