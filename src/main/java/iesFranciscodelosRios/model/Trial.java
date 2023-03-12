2a722ee29a39352610bbfa7d30529fedba0f1d28
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-12 12:51:33 +0100

    0.3 Primer Sprint finalizado

--- src/main/java/iesFranciscodelosRios/model/Trial.java
+++ src/main/java/iesFranciscodelosRios/model/Trial.java
@@ -5,11 +5,9 @@ import iesFranciscodelosRios.Enum.Kit;
 import iesFranciscodelosRios.Enum.Type;
 import iesFranciscodelosRios.interfaces.*;
 
-import javax.naming.LimitExceededException;
 import java.util.*;
 
 public final class Trial implements iTrial {
-    private final iParticipation i = new Participation();
     private Type type;
     private Category category;
     private Kit kit;
@@ -28,57 +26,69 @@ public final class Trial implements iTrial {
     /**
      * Este metodo analizara las puntuaciones de todos los gimnastas y los agrupara en un arrayList. acto seguido
      * analiza el arrayList y elimina los gimnastas con menor puntuacion
+     *
      * @return el ArrayList con los participantes con maxima puntuacion. Si es mayor a 0. si no devuelve null
      */
     public ArrayList<Participation> getWinner() {
         ArrayList<Participation> maxPoints = new ArrayList<>();
         maxPoints.add(new Participation());
-        try {
-            for (Participation aux : participations.values()) {
-                if (aux.getPoints() >= maxPoints.get(maxPoints.size() - 1).getPoints() && aux.getPoints() != 0) {
-                    maxPoints.add(aux);
-                    if(maxPoints.get(maxPoints.indexOf(aux)-1)!=null){
-                        if(maxPoints.get(maxPoints.indexOf(aux)-1).getPoints()<aux.getPoints()){
-                            maxPoints.remove(maxPoints.indexOf(aux)-1);
-                        }
+        for (Participation aux : participations.values()) {
+            if (aux.getPoints() >= maxPoints.get(maxPoints.size() - 1).getPoints() && aux.getPoints() != 0) {
+                maxPoints.add(aux);
+                if (maxPoints.get(maxPoints.indexOf(aux) - 1) != null) {
+                    if (maxPoints.get(maxPoints.indexOf(aux) - 1).getPoints() < aux.getPoints()) {
+                        maxPoints.remove(maxPoints.indexOf(aux) - 1);
                     }
                 }
             }
-            if(maxPoints.get(0)!=null && maxPoints.get(0).getPoints()>0){
-                return maxPoints;
-            }
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        } catch (IndexOutOfBoundsException e) {
-            e.printStackTrace();
+        }
+        if (maxPoints.get(0) != null && maxPoints.get(0).getPoints() > 0) {
+            return maxPoints;
         }
         return null;
     }
 
     /**
-     * @param b
-     * @return
+     *  Agrega una participacion si esta no existe ya en la coleccion
+     * @param b participacion
+     * @return devuelve true i lo ha agregado y false en caso contrario
      */
     @Override
     public boolean addParticipant(Participation b) {
         if (b != null) {
-            for (Participation c : participations.values()) {
-                if (c.getDorsal() == b.getDorsal()) {
-                    return false;
-                }
+            if (!participations.containsKey(b.getDorsal())) {
+                participations.put(b.getDorsal(), b);
+                return true;
             }
-            participations.put(b.getDorsal(), b);
-            return true;
         }
         return false;
     }
 
-    public boolean score() {
+    /**
+     * Si se cumple el criterio de puntuacion se le asignara al usuario con el dorsal asignado
+     * la puntuacion correspondiente a su actuacion.
+     * este metodo deberia ser usado por un jurado
+     * @param points puntos asignados al participante
+     * @return devuelve true si se cumple el criterio y false en caso contrario
+     */
+    public boolean score(Integer dorsal,int points) {
+        if(searchParticipant(dorsal)!=null){
+            if(points<=10 && points>=0){
+                searchParticipant(dorsal).setPoints(points);
+                return true;
+            }
+        }
         return false;
     }
 
-    public ArrayList<Participation> showAllParticipants() {
-        return null;
+    /**
+     * Metodo el cual buscara por la clave primaria si el objeto existe en la lista. si lo encuentra devuelve
+     * el objeto . si no devuelve null
+     * @param dorsal clave primaria para buscar . debe ser el dorsal del participante
+     * @return devuelve el objeto encontrado o null
+     */
+    public Participation searchParticipant(Integer dorsal){
+        return participations.get(dorsal);
     }
 
     public Type getType() {
@@ -95,7 +105,6 @@ public final class Trial implements iTrial {
 
     /**
      * En principio este setParticipations lo he creado para poder usarlo en los test pero tal vez deberia ser eliminado
-     * al igual que el getParticipations
      */
     public void setParticipations(HashMap<Integer, Participation> p) {
         this.participations = p;
