0f1cc9bb0f45e707a179a6825b94717fbb545d66
isabelroldan <38252719+isabelroldan@users.noreply.github.com>
2023-03-13 16:26:00 +0100

    Add files via upload

--- src/main/java/gymnasticCompetititons/model/Competition.java
+++ src/main/java/gymnasticCompetititons/model/Competition.java
@@ -6,8 +6,6 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import model.Product;
-
 public class Competition {
 	private String name;
 	private String description;
@@ -17,6 +15,100 @@ public class Competition {
 	Map<Integer, Participation> participations = new HashMap<>();
 
 
+	public Competition(String name, String description, String initialDate) {
+		this.name = name;
+		this.description = description;
+		this.initialDate = initialDate;
+	}
+
+
+	public Competition() {
+		this("","","");
+	}
+
+	
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getInitialDate() {
+		return initialDate;
+	}
+
+
+	public void setInitialDate(String initialDate) {
+		this.initialDate = initialDate;
+	}
+
+
+	public ArrayList<Trial> getTrials() {
+		return trials;
+	}
+
+
+	public void setTrials(ArrayList<Trial> trials) {
+		this.trials = trials;
+	}
+
+
+	public Map<Integer, Participation> getParticipations() {
+		return participations;
+	}
+
+
+	public void setParticipations(Map<Integer, Participation> participations) {
+		this.participations = participations;
+	}
+
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Competition other = (Competition) obj;
+		return Objects.equals(name, other.name);
+
+	@Override
+	public String toString() {
+		return "Competition "
+					+ "\n\tName:" + name
+					+ "\n\tDescription=" + description  
+					+ "\n\tInitial date=" + initialDate ;
+	}
+
+	/**
+	 * Add a trial
+	 * @param trial add
+	 * @return True if it has been added successfully and false if not
+	 */
 	public boolean addTrial(Trial trial) {
 		boolean result = false;
 		if(trial!=null && !isTrial(trial)) {
@@ -27,7 +119,13 @@ public class Competition {
 		return result;
 	}
 
-	
+	/**
+	 * To see a trial
+	 * @param type: trial key
+	 * @param category: trial key
+	 * @param kit: trial key
+	 * @return The trial sought by the parameters
+	 */
 	public Trial showTrial(String type, String category, String kit) {
 		Trial trial = null;
 		if(trials!=null) {
@@ -46,10 +144,15 @@ public class Competition {
 		return trial;
 	}
 	
+	/**
+	 * Method to change a trial
+	 * @param trial to change
+	 * @return true if the change was successful and false if not
+	 */
 	public boolean changeTrial(Trial trial) {
 		boolean result = false;
 		
-		if(trial != null  &&  showArticle(trial.getType().getCategory().getKit())!=null) {
+		if(trial != null  &&  showTrial(trial.getType().getCategory().getKit())!=null) {
 			Iterator<Trial> it = trials.iterator();
 			while(it.hasNext()){
 				Trial trials = it.next();
@@ -63,6 +166,11 @@ public class Competition {
 		return result;
 	}
 	
+	/**
+	 * Method to delete a trial
+	 * @param trial to delete
+	 * @return true if the delete was successful and false if not
+	 */
 	public boolean removeTrial(Trial trial) {
 		boolean result = false;
 		
@@ -80,8 +188,12 @@ public class Competition {
 		
 		return result;
 	}
-	
-	
+	 
+	/**
+	 * Look for trial
+	 * @param trial to search
+	 * @return true if found and false if not
+	 */
 	public boolean isTrial(Trial trial) {
 		boolean result = false;
 		Iterator<Trial> it = trials.iterator();
@@ -97,7 +209,12 @@ public class Competition {
 		return result;
 	}
 	
-	
+	/**
+	 * Add a participation
+	 * @param participation add
+	 * @param key add
+	 * @return True if it has been added successfully and false if not
+	 */
 	public boolean addParticipation(Participation participation, int key) {
 		boolean result = false;
 		if(participation!=null && !isParticipation(participation)) {
@@ -108,6 +225,11 @@ public class Competition {
 		return result;
 	}
 	
+	/**
+	 * To see a participation
+	 * @param dorsalNumber participation key
+	 * @return The participation sought by the parameters
+	 */
 	public Participation showParticipation(int dorsalNumber) {
 			
 		Participation participation = null;
@@ -120,6 +242,9 @@ public class Competition {
 		return participation;
 	}
 	
+	/**
+	 * To see all participations
+	 */
 	public void showAllParticipation(){
 		Participation participation = null;
 		Iterator<String> it = participations.keySet().iterator();//tambien se puede recorer con un forEach
@@ -129,7 +254,12 @@ public class Competition {
 			System.out.println(participations.get(key));
 		}
 	}
-	
+	 
+	/**
+	 * Method to delete a participation
+	 * @param participation to delete
+	 * @return true if the delete was successful and false if not
+	 */
 	public boolean removeParticipation(Participation participation, int key) {
 		boolean result = false;
 		if(participation!=null && !isProduct(participation)) {
@@ -139,7 +269,12 @@ public class Competition {
 		
 		return result;
 	}
-	
+	 
+	/**
+	 * Method to change a particpation
+	 * @param participation to change
+	 * @return true if the change was successful and false if not
+	 */
 	public boolean changeParticipation(Map<Integer, Participation> participation) {
 		boolean result = false;
 		
@@ -156,7 +291,12 @@ public class Competition {
 		
 		return result;
 	}
-	
+	 
+	/**
+	 * Look for participation
+	 * @param participation to search
+	 * @return true if found and false if not
+	 */
 	public boolean isParticipation(Participation participation) {
 		boolean result = false;
 		Iterator<Integer> it = participations.keySet().iterator();
