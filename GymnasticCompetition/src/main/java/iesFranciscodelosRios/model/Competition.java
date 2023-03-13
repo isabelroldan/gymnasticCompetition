de64a1be7808b7779e597e23de76b0f6ce9739d2
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-13 20:57:37 +0100

    0.1

---
+++ GymnasticCompetition/src/main/java/iesFranciscodelosRios/model/Competition.java
@@ -0,0 +1,335 @@
+package iesFranciscodelosRios.model;
+
+import java.util.*;
+
+import iesFranciscodelosRios.Enum.Category;
+import iesFranciscodelosRios.Enum.Kit;
+import iesFranciscodelosRios.Enum.Type;
+import iesFranciscodelosRios.model.*;
+
+public class Competition {
+
+	private String name;
+	private String description;
+	private String initialDate;
+	ArrayList<Trial> trials = new ArrayList<Trial>();
+	Map<Integer,Participation> participations = new HashMap<>();
+
+
+	public Competition(String name, String description, String initialDate) {
+		this.name = name;
+		this.description = description;
+		this.initialDate = initialDate;
+	}
+
+
+	public Competition() {
+		this("","","");
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getInitialDate() {
+		return initialDate;
+	}
+
+
+	public void setInitialDate(String initialDate) {
+		this.initialDate = initialDate;
+	}
+
+
+	public ArrayList<Trial> getTrials() {
+		return trials;
+	}
+
+
+	public void setTrials(ArrayList<Trial> trials) {
+		this.trials = trials;
+	}
+
+
+	public Map<Integer, Participation> getParticipations() {
+		return participations;
+	}
+
+
+	public void setParticipations(Map<Integer, Participation> participations) {
+		this.participations = participations;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Competition other = (Competition) obj;
+		return Objects.equals(name, other.name);
+	}
+		@Override
+		public String toString() {
+			return "Competition "
+					+ "\n\tName:" + name
+					+ "\n\tDescription=" + description
+					+ "\n\tInitial date=" + initialDate ;
+		}
+
+		/**
+		 * Add a trial
+		 * @param trial add
+		 * @return True if it has been added successfully and false if not
+		 */
+		public boolean addTrial(Trial trial) {
+			boolean result = false;
+			if(trial!=null && !isTrial(trial)) {
+				trials.add(trial);
+				result = true;
+			}
+
+			return result;
+		}
+
+		/**
+		 * To see a trial
+		 * @param type: trial key
+		 * @param category: trial key
+		 * @param kit: trial key
+		 * @return The trial sought by the parameters
+		 */
+		public Trial showTrial(String type, String category, String kit) {
+			Trial trial = null;
+			if (trials != null) {
+				Iterator<Trial> it = trials.iterator();
+				while (it.hasNext()) {
+					Trial Trial = it.next();
+					if (trial.getType().equals(type) &&
+							trial.getCategory().equals(category) &&
+							trial.getKit().equals(kit)) {
+						return trial;
+					}
+				}
+			}
+   		return trial;
+		}
+
+		/**
+		 * Method to change a trial
+		 * @param trial to change
+		 * @return true if the change was successful and false if not
+		 */
+		public boolean changeTrial(Trial trial) {
+			boolean result = false;
+
+			if (trial != null) {
+				Type trialType = trial.getType();
+				if (trialType != null) {
+					Category trialCategory = trial.getCategory();
+					if (trialCategory != null) {
+						Kit trialKit = trial.getKit();
+						if (trialKit != null) {
+							if (showTrial(trial.getType().name(),trial.getCategory().name(),trial.getKit().name()) != null) {
+								Iterator<Trial> it = trials.iterator();
+								while(it.hasNext()){
+									Trial trials = it.next();
+									if (trials.equals(trial)) {
+										trials = trial;
+										result = true;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			return result;
+		}
+
+		/**
+		 * Method to delete a trial
+		 * @param trial to delete
+		 * @return true if the delete was successful and false if not
+		 */
+		public boolean removeTrial(Trial trial) {
+			boolean result = false;
+
+			if (trial != null) {
+				Type trialType = trial.getType();
+				if (trialType != null) {
+					Category trialCategory = trial.getCategory();
+					if (trialCategory != null) {
+						Kit trialKit = trial.getKit();
+						if (trialKit != null) {
+							if (showTrial(trial.getType().name(),trial.getCategory().name(),trial.getKit().name()) != null) {
+								Iterator<Trial> it = trials.iterator();
+								while(it.hasNext()){
+									Trial trials = it.next();
+									if (trials.equals(trial)) {
+										it.remove();
+										result = true;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			return result;
+		}
+
+		/**
+		 * Look for trial
+		 * @param trial to search
+		 * @return true if found and false if not
+		 */
+		public boolean isTrial(Trial trial) {
+			boolean result = false;
+			Iterator<Trial> it = trials.iterator();
+			while(it.hasNext()){
+				Trial trials = it.next();
+				if (trials!=null && trial.equals(trials)) {
+					result = true;
+					break;
+				}
+
+			}
+
+			return result;
+		}
+
+		/**
+		 * Add a participation
+		 * @param participation add
+		 * @param key add
+		 * @return True if it has been added successfully and false if not
+		 */
+		public boolean addParticipation(Participation participation, int key) {
+			boolean result = false;
+			if(participation!=null && !isParticipation(participation)) {
+				participations.put(participation.getDorsal(), participation);
+				result = true;
+			}
+
+			return result;
+		}
+
+		/**
+		 * To see a participation
+		 * @param dorsalNumber participation key
+		 * @return The participation sought by the parameters
+		 */
+		public Participation showParticipation(int dorsalNumber) {
+
+			Participation participation = null;
+			Iterator<Integer> it = participations.keySet().iterator();
+			while(it.hasNext()){
+				Integer key = it.next();
+				participation = participations.get(dorsalNumber);
+			}
+
+			return participation;
+		}
+
+		/**
+		 * To see all participations
+		 */
+		public void showAllParticipation(){
+			Participation participation = null;
+			Iterator<Integer> it = participations.keySet().iterator();//tambien se puede recorer con un forEach
+			while(it.hasNext()) {
+				Integer key = it.next();
+				//esto te da el valor perteneciente a esa key:
+				System.out.println(participations.get(key));
+			}
+		}
+
+		/**
+		 * Method to delete a participation
+		 * @param participation to delete
+		 * @return true if the delete was successful and false if not
+		 */
+		public boolean removeParticipation(Participation participation, int key) {
+			boolean result = false;
+			if(participation!=null && !isParticipation(participation)) {
+				participations.remove(participation.getDorsal(), participation);
+				result = true;
+			}
+
+			return result;
+		}
+
+		/**
+		 * Method to change a particpation
+		 * @param participation to change
+		 * @return true if the change was successful and false if not
+		 */
+		public boolean changeParticipation(Participation participation) {
+			boolean result = false;
+
+			if(participation != null  &&  showParticipation((participation).getDorsal())!=null) {
+				Iterator<Integer> it = participations.keySet().iterator();
+				while(it.hasNext()){
+					Integer key = it.next();
+					if (participations.equals(participation)) {
+						participations.put(participation.getDorsal(),participation);
+						result = true;
+					}
+				}
+			}
+
+			return result;
+		}
+
+		/**
+		 * Look for participation
+		 * @param participation to search
+		 * @return true if found and false if not
+		 */
+		public boolean isParticipation(Participation participation) {
+			boolean result = false;
+			Iterator<Integer> it = participations.keySet().iterator();
+			while(it.hasNext()){
+				Integer key = it.next();
+				if (participations!=null && participation.equals(participations)) {
+					result = true;
+					break;
+				}
+
+			}
+
+			return result;
+		}
+
+}
---
+++ GymnasticCompetition/src/main/java/iesFranciscodelosRios/model/Trial.java
@@ -0,0 +1,124 @@
+package iesFranciscodelosRios.model;
+
+import iesFranciscodelosRios.Enum.Category;
+import iesFranciscodelosRios.Enum.Kit;
+import iesFranciscodelosRios.Enum.Type;
+import iesFranciscodelosRios.interfaces.*;
+
+import java.util.*;
+
+public final class Trial implements iTrial {
+    private Type type;
+    private Category category;
+    private Kit kit;
+    private HashMap<Integer, Participation> participations = new HashMap<>();
+
+    public Trial() {
+
+    }
+
+    public Trial(Type type, Category category, Kit kit) {
+        this.type = type;
+        this.category = category;
+        this.kit = kit;
+    }
+
+    /**
+     * Este metodo analizara las puntuaciones de todos los gimnastas y los agrupara en un arrayList. acto seguido
+     * analiza el arrayList y elimina los gimnastas con menor puntuacion
+     *
+     * @return el ArrayList con los participantes con maxima puntuacion. Si es mayor a 0. si no devuelve null
+     */
+    public ArrayList<Participation> getWinner() {
+        ArrayList<Participation> maxPoints = new ArrayList<>();
+        maxPoints.add(new Participation());
+        for (Participation aux : participations.values()) {
+            if (aux.getPoints() >= maxPoints.get(maxPoints.size() - 1).getPoints() && aux.getPoints() != 0) {
+                maxPoints.add(aux);
+                if (maxPoints.get(maxPoints.indexOf(aux) - 1) != null) {
+                    if (maxPoints.get(maxPoints.indexOf(aux) - 1).getPoints() < aux.getPoints()) {
+                        maxPoints.remove(maxPoints.indexOf(aux) - 1);
+                    }
+                }
+            }
+        }
+        if (maxPoints.get(0) != null && maxPoints.get(0).getPoints() > 0) {
+            return maxPoints;
+        }
+        return null;
+    }
+
+    /**
+     *  Agrega una participacion si esta no existe ya en la coleccion
+     * @param b participacion
+     * @return devuelve true i lo ha agregado y false en caso contrario
+     */
+    @Override
+    public boolean addParticipant(Participation b) {
+        if (b != null) {
+            if (!participations.containsKey(b.getDorsal())) {
+                participations.put(b.getDorsal(), b);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Si se cumple el criterio de puntuacion se le asignara al usuario con el dorsal asignado
+     * la puntuacion correspondiente a su actuacion.
+     * este metodo deberia ser usado por un jurado
+     * @param points puntos asignados al participante
+     * @return devuelve true si se cumple el criterio y false en caso contrario
+     */
+    public boolean score(Integer dorsal,int points) {
+        if(searchParticipant(dorsal)!=null){
+            if(points<=10 && points>=0){
+                searchParticipant(dorsal).setPoints(points);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Metodo el cual buscara por la clave primaria si el objeto existe en la lista. si lo encuentra devuelve
+     * el objeto . si no devuelve null
+     * @param dorsal clave primaria para buscar . debe ser el dorsal del participante
+     * @return devuelve el objeto encontrado o null
+     */
+    public Participation searchParticipant(Integer dorsal){
+        return participations.get(dorsal);
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public Kit getKit() {
+        return kit;
+    }
+
+    /**
+     * En principio este setParticipations lo he creado para poder usarlo en los test pero tal vez deberia ser eliminado
+     */
+    public void setParticipations(HashMap<Integer, Participation> p) {
+        this.participations = p;
+    }
+
+    public HashMap<Integer, Participation> getParticipations() {
+        return participations;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Trial trial = (Trial) o;
+        return type == trial.type && category == trial.category && kit == trial.kit;
+    }
+}
