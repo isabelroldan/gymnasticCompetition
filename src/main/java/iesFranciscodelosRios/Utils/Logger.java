693c571b3366cfd9b1d482cd814d1b926deaf27a
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-14 14:13:47 +0100

    0.2

--- src/main/java/iesFranciscodelosRios/Utils/Logger.java
+++ src/main/java/iesFranciscodelosRios/Utils/Logger.java
@@ -4,7 +4,16 @@ import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.SimpleFormatter;
 
+
+import java.util.logging.Level;
 public class Logger {
+
+
+    public static java.util.logging.Logger CreateLogger(String filename) {
+        java.util.logging.Logger logger= java.util.logging.Logger.getLogger(filename);
+        logger.setLevel(Level.OFF);
+        return logger;
+    }
     public static void CreateFile(java.util.logging.Logger logger, String filename){
         FileHandler fileHandler=null;
         try{
