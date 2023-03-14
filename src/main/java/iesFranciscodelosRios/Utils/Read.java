c4d712492e48b4038590d3fa966f98abd11c7521
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-14 19:48:20 +0100

    0.2
    agrega el metodo readMail y readTelephoneNumber

--- src/main/java/iesFranciscodelosRios/Utils/Read.java
+++ src/main/java/iesFranciscodelosRios/Utils/Read.java
@@ -1,57 +1,61 @@
 package iesFranciscodelosRios.Utils;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-public class Read{
-    private final static Scanner sc=new Scanner(System.in);
+public class Read {
+    private final static Scanner sc = new Scanner(System.in);
+
     /**
      * This method will read an integer
+     *
      * @param msn receive the message to print
      * @return return a reader number
      */
-    public static int readInt(String msn){
-        int numero=-1;
-        boolean isCorrect=false;
-        do{
-            try{
-                System.out.println(Utils.amarillo+msn);
-                numero=sc.nextInt();
-                isCorrect=true;
-            }catch (Exception e){
-                System.out.println(Utils.rojo+"Enter a valid number"+Utils.b);
+    public static int readInt(String msn) {
+        int numero = -1;
+        boolean isCorrect = false;
+        do {
+            try {
+                System.out.println(Utils.amarillo + msn);
+                numero = sc.nextInt();
+                isCorrect = true;
+            } catch (Exception e) {
+                System.out.println(Utils.rojo + "Enter a valid number" + Utils.b);
             }
             sc.nextLine();
-        }while(!isCorrect);
+        } while (!isCorrect);
         return numero;
     }
 
     /**
      * This method will read a string of characters.
+     *
      * @param msn receive the message to print
      * @return return a String
      */
-    public static String readString(String msn){
-        Scanner sc=new Scanner(System.in);
-        String evento=null;
-        try{
-            System.out.println(Utils.amarillo+msn);
-            evento=sc.nextLine();
-            evento=evento.toUpperCase();
+    public static String readString(String msn) {
+        Scanner sc = new Scanner(System.in);
+        String evento = null;
+        try {
+            System.out.println(Utils.amarillo + msn);
+            evento = sc.nextLine();
+            evento = evento.toUpperCase();
 
-        }catch (Exception e){
-            System.out.println(Utils.rojo+"Enter the option correct"+Utils.b);
+        } catch (Exception e) {
+            System.out.println(Utils.rojo + "Enter the option correct" + Utils.b);
         }
         return evento;
     }
 
-    public static double readDouble(String msn){
-        double result=-1.0;
-        try{
+    public static double readDouble(String msn) {
+        double result = -1.0;
+        try {
             System.out.println(msn);
-            result= sc.nextDouble();
-        }catch (Exception e){
+            result = sc.nextDouble();
+        } catch (Exception e) {
             System.out.println(e);
         }
         return result;
@@ -59,6 +63,7 @@ public class Read{
 
     /**
      * Metodo el cual leera una fecha para retornarla en el formato correspondiente
+     *
      * @param msn Mensaje a imprimir
      * @return fecha completa
      */
@@ -68,13 +73,13 @@ public class Read{
         Date date = null;
         boolean success = false;
         while (!success) {
-            System.out.println(Utils.amarillo+msn+" Enter a date in format dd/MM/yyyy:"+Utils.b);
+            System.out.println(Utils.amarillo + msn + " Enter a date in format dd/MM/yyyy:" + Utils.b);
             String inputDate = sc.nextLine();
             try {
                 date = dateFormat.parse(inputDate);
                 success = true;
             } catch (ParseException e) {
-                System.out.println(Utils.rojo+"Invalid date format, please try again."+Utils.b);
+                System.out.println(Utils.rojo + "Invalid date format, please try again." + Utils.b);
             }
         }
         return date;
@@ -83,47 +88,48 @@ public class Read{
     /**
      * Metodo que leera un dni a partir de que cumpla las requisitos de un dni. si no volvera a pedirlo. Este metodo
      * es capaz de calcular la letra del dni si el usuario no lo introduce
+     *
      * @param msn Mensaje que se leera
      * @return retorna el dni completo
      */
-    public static String readDNI(String msn){
+    public static String readDNI(String msn) {
         String dni;
         boolean isCorrect;
-        do{
-            isCorrect=true;
-            System.out.println(Utils.amarillo+msn+Utils.b);
-            dni=sc.nextLine();
-            dni=dni.toUpperCase();
-            if (dni.length()==9){ //buscara que todos los numeros sean numeros
-                String numerosDNI="";
-                for(int c=0;c<dni.length()-1 && isCorrect;c++){
-                    isCorrect=isNumber(dni.charAt(c));
-                    numerosDNI+=dni.charAt(c);//esto los almacena correctamente
+        do {
+            isCorrect = true;
+            System.out.println(Utils.amarillo + msn + Utils.b);
+            dni = sc.nextLine();
+            dni = dni.toUpperCase();
+            if (dni.length() == 9) { //buscara que todos los numeros sean numeros
+                String numerosDNI = "";
+                for (int c = 0; c < dni.length() - 1 && isCorrect; c++) {
+                    isCorrect = isNumber(dni.charAt(c));
+                    numerosDNI += dni.charAt(c);//esto los almacena correctamente
                 }
-                if(isCorrect){ //si los 8 primeros digitos so5n numeros entonces se comprueba que la letra sea correcta
-                    if(dni.charAt(8)== calculateLetterDNI(numerosDNI)){
-                        isCorrect=true;
-                    }else{
-                        isCorrect=false;
+                if (isCorrect) { //si los 8 primeros digitos so5n numeros entonces se comprueba que la letra sea correcta
+                    if (dni.charAt(8) == calculateLetterDNI(numerosDNI)) {
+                        isCorrect = true;
+                    } else {
+                        isCorrect = false;
                     }
                 }
-            }else if(dni.length()==8){
-                boolean isTrue=Utils.confirm("La letra es "+ calculateLetterDNI(dni)+" ?");
-                if(isTrue){
-                    dni+= calculateLetterDNI(dni);
-                }else{
-                    isCorrect=false;
+            } else if (dni.length() == 8) {
+                boolean isTrue = Utils.confirm("La letra es " + calculateLetterDNI(dni) + " ?");
+                if (isTrue) {
+                    dni += calculateLetterDNI(dni);
+                } else {
+                    isCorrect = false;
                 }
-            } else if (dni.length()==8 || dni.length()==9){
-                System.out.println(Utils.rojo+"Introduce un dni que no este registrado"+ Utils.b);
-                isCorrect=false;
-            }else{
-                isCorrect=false;
+            } else if (dni.length() == 8 || dni.length() == 9) {
+                System.out.println(Utils.rojo + "Introduce un dni que no este registrado" + Utils.b);
+                isCorrect = false;
+            } else {
+                isCorrect = false;
             }
-            if (dni.length()!=8 && dni.length()!=9 || !isCorrect) {
-                System.out.println(Utils.rojo+"Introduzca correctamente el DNI"+ Utils.b);
+            if (dni.length() != 8 && dni.length() != 9 || !isCorrect) {
+                System.out.println(Utils.rojo + "Introduzca correctamente el DNI" + Utils.b);
             }
-        }while(!isCorrect);
+        } while (!isCorrect);
 
         return dni;
     }
@@ -131,11 +137,12 @@ public class Read{
     /**
      * Metodo que es usado por leeDNI. su objetivo es comprobar que todos los supuestos numeros escritos por el usuario son caracteres del 0-9
      * si no retorna un false a leeDNI haciendo que este vuelva a pedir el dni
+     *
      * @param c comprueba que el caracter sea un numero
      * @return retorna
      */
-    public static boolean isNumber(char c){ //funcion que calcula q los valores introducidos en dni sean todos numeros
-        if(c>='0' && c<='9'){
+    public static boolean isNumber(char c) { //funcion que calcula q los valores introducidos en dni sean todos numeros
+        if (c >= '0' && c <= '9') {
             return true;
         }
         return false;
@@ -144,12 +151,41 @@ public class Read{
     /**
      * Metodo que sera utilizado por leeDNI con 2 objetivos . o bien comprobar que la letra del dni sea correspondiente al dni introducido
      * o si el usuario no introduce la letra del dni . el programa le sujiera la letra
+     *
      * @param numeroDNI Importa los numeros del dni
      * @return retorna la letra calculada
      */
-    public static char calculateLetterDNI(String numeroDNI){ //
-        char[] letras={'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
-        char letra= letras [Integer.valueOf(numeroDNI) % 23]; //Integer.valueOF(numeroDNI)  coge la cadena string NumeroDNI lo convertira a numerico (como si fuese un int)
+    public static char calculateLetterDNI(String numeroDNI) { //
+        char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        char letra = letras[Integer.valueOf(numeroDNI) % 23]; //Integer.valueOF(numeroDNI)  coge la cadena string NumeroDNI lo convertira a numerico (como si fuese un int)
         return letra;
+    }
+
+    public static boolean findOutDate(String date) {
+        try {
+            SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
+            formatDate.setLenient(false);
+            formatDate.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+    public static String readTelephoneNumber(){
+        String result=null;
+        String telephone=readString("Enter a valid telephone number with a code number [+01 123456789]");
+        if(telephone.matches("\\+?[0-9]{2} ?[0-9]{9,11}")){
+            result= telephone;
+        }
+        return result;
+    }
+    public static String readMail(){
+        String result=null;
+        String mail=readString("Enter a valid mail [example@mail.com");
+        if(mail.matches("/[a-zA-Z]*[0-9]?@[a-zA-Z]*.[A-Za-z]{2,3}")){
+            result=mail;
+        }
+        return result;
     }
 }
