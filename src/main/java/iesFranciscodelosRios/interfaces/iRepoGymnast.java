a5125964069548d7d72a6aa6b3ec87c439ac35f3
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-15 20:55:43 +0100

    Update iRepoGymnast.java

--- src/main/java/iesFranciscodelosRios/interfaces/iRepoGymnast.java
+++ src/main/java/iesFranciscodelosRios/interfaces/iRepoGymnast.java
@@ -1,11 +1,7 @@
 package iesFranciscodelosRios.interfaces;
 import iesFranciscodelosRios.model.Gymnast;
-import iesFranciscodelosRios.model.Person;
-import java.util.ArrayList;
-
 public interface iRepoGymnast {
-	boolean addGymnast(Gymnast gymnast);
+	boolean addGymnast(Gymnast gym);
 	boolean deleteGymnast(String DNI);
-	Gymnast showGymnast(String DNI);
-	void ShowAll();
+	Gymnast showGymnast(String dni);
 }
