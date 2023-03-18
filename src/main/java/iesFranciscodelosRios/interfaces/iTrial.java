8608810065878d6634329ee71175799db33fc42e
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-18 11:41:19 +0100

    0.2 18_03_2023

--- src/main/java/iesFranciscodelosRios/interfaces/iTrial.java
+++ src/main/java/iesFranciscodelosRios/interfaces/iTrial.java
@@ -4,7 +4,7 @@ import iesFranciscodelosRios.model.Participation;
 import java.util.ArrayList;
 
 public interface iTrial {
-    ArrayList<Participation> getWinner();
+    ArrayList<ArrayList<Participation>> getWinner();
     boolean addParticipant(Participation b);
     boolean score(Integer dorsal,int points);
     Participation searchParticipant(Integer dorsal);
