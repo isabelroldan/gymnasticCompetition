74a480dff6e38713f48725a5f266c7454d6be920
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-18 11:40:08 +0100

    0.6 18_03_2023
    getWinner actualizado y refactorizado. ademas el codigo ha sido documentado

--- src/main/java/iesFranciscodelosRios/model/Trial.java
+++ src/main/java/iesFranciscodelosRios/model/Trial.java
@@ -4,11 +4,12 @@ import iesFranciscodelosRios.Enum.Category;
 import iesFranciscodelosRios.Enum.Kit;
 import iesFranciscodelosRios.Enum.Type;
 import iesFranciscodelosRios.interfaces.*;
+
 import java.util.*;
 import java.util.logging.Logger;
 
 public final class Trial implements iTrial {
-    private final static Logger logger= iesFranciscodelosRios.Utils.Logger.CreateLogger("model.Trial");
+    private final static Logger logger = iesFranciscodelosRios.Utils.Logger.CreateLogger("model.Trial");
     private Type type;
     private Category category;
     private Kit kit;
@@ -25,27 +26,50 @@ public final class Trial implements iTrial {
     }
 
     /**
-     * Este metodo analizara las puntuaciones de todos los gimnastas y los agrupara en un arrayList. acto seguido
-     * analiza el arrayList y elimina los gimnastas con menor puntuacion
-     *
-     * @return el ArrayList con los participantes con maxima puntuacion. Si es mayor a 0. si no devuelve null
+     * Este metodo genera 3 colecciones donde almacenar los ganadores .el proceso para saber la posicion de un
+     * participante es al comprobar si la puntuacion del siguiente participante en una coleccion ordenada
+     * es igual a la del anterior participante (que al estar ordenada la lista es el que mayor puntuacion tendra).
+     * @return devuelve un arrayList con 3 colecciones en las cuales se almacenan los participantes ganadores segun su
+     * posicion de puntuacion
      */
-    public ArrayList<Participation> getWinner() {
-        ArrayList<Participation>result=null;
-        ArrayList<Participation> aux=new ArrayList<>();
-        try{
-            for (Integer key : participations.keySet()){
-                aux.add(participations.get(key));
+    public ArrayList<ArrayList<Participation>> getWinner() {
+        ArrayList<ArrayList<Participation>> result=null;
+        try {
+            ArrayList<Participation> sortedList = new ArrayList<>(participations.values());
+            sortedList.removeIf(p -> p.getPoints() == 0);
+            Collections.sort(sortedList);
+            int position = 0;
+            ArrayList<Participation> first = new ArrayList<>();
+            ArrayList<Participation> second = new ArrayList<>();
+            ArrayList<Participation> third = new ArrayList<>();
+            for (int i = 0; i < sortedList.size() && position <= 2; i++) {
+                Participation currentParticipation = sortedList.get(i);
+                switch (position) {
+                    case 0:
+                        first.add(currentParticipation);
+                        break;
+                    case 1:
+                        second.add(currentParticipation);
+                        break;
+                    case 2:
+                        third.add(currentParticipation);
+                        break;
+                }
+                if (i + 1 < sortedList.size() && sortedList.get(i + 1).getPoints() != currentParticipation.getPoints()) {
+                    position++;
+                }
             }
-            Collections.sort(aux);
-            result=aux;
-        }catch (NullPointerException e){
+            result=new ArrayList<>();
+            result.add(first);
+            result.add(second);
+            result.add(third);
+        } catch (NullPointerException e) {
             e.printStackTrace();
             logger.severe("Error. method getWinner. The participating collection is null");
-        }catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             logger.severe("Error. method getWinner. The participation array is smaller than desired");
         } finally {
-            if(result==null){
+            if (result == null) {
                 logger.warning("Warning. method getWinner. It has not been executed correctly");
             }
         }
@@ -53,66 +77,70 @@ public final class Trial implements iTrial {
     }
 
     /**
-     *  Agrega una participacion si esta no existe ya en la coleccion
+     * Agrega una participacion si esta no existe ya en la coleccion
+     *
      * @param b participacion
      * @return devuelve true i lo ha agregado y false en caso contrario
      */
     @Override
     public boolean addParticipant(Participation b) {
-        boolean end=false;
-        try{
+        boolean end = false;
+        try {
             if (b != null) {
                 if (!participations.containsKey(b.getDorsal())) {
                     participations.put(b.getDorsal(), b);
                     logger.info("OK. method addParticipant. it was executed correctly");
-                    end=true;
+                    end = true;
                 }
             }
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
             e.printStackTrace();
             logger.severe("Error. method addParticipant. The participating collection is null");
-        }finally {
-            if(!end){
+        } finally {
+            if (!end) {
                 logger.warning("Warning. method addParticipant. It has not been executed correctly");
             }
         }
         return end;
     }
-    public boolean login(Judge j,String dni,char[] password){
-        boolean result=false;
-        try{
-            if(j.getDNI().equalsIgnoreCase(dni) && Arrays.equals(j.getPassword(),password)){
-                result=true;
+
+    public boolean login(Judge j, String dni, char[] password) {
+        boolean result = false;
+        try {
+            if (j.getDNI().equalsIgnoreCase(dni) && Arrays.equals(j.getPassword(), password)) {
+                result = true;
             }
-        }catch (NullPointerException e) {
+        } catch (NullPointerException e) {
             logger.severe("Error " + e.getMessage());
         }
         return result;
     }
+
     /**
      * Si se cumple el criterio de puntuacion se le asignara al usuario con el dorsal asignado
      * la puntuacion correspondiente a su actuacion.
      * este metodo deberia ser usado por un jurado
+     *
      * @param points puntos asignados al participante
      * @return devuelve true si se cumple el criterio y false en caso contrario
      */
-    public boolean score(Integer dorsal,int points) {
-        boolean end=false;
+    public boolean score(Integer dorsal, int points) {
+        boolean end = false;
         try {
-            if(searchParticipant(dorsal)!=null){
-                if(points<=10 && points>=0){
+            if (searchParticipant(dorsal) != null) {
+                if (points <= 10 && points >= 0) {
                     searchParticipant(dorsal).setPoints(points);
-                    end=true;
+                    end = true;
                 }
             }
-        }catch (ArithmeticException e){
+        } catch (ArithmeticException e) {
             e.printStackTrace();
             logger.severe("Error. method score. An arithmetic error occurred");
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
             e.printStackTrace();
             logger.severe("Error. method score. The participating collection is null");
         } finally {
-            if(!end){
+            if (!end) {
                 logger.warning("Warning. method score. It has not been executed correctly");
             }
         }
@@ -122,13 +150,14 @@ public final class Trial implements iTrial {
     /**
      * Metodo el cual buscara por la clave primaria si el objeto existe en la lista. si lo encuentra devuelve
      * el objeto . si no devuelve null
+     *
      * @param dorsal clave primaria para buscar . debe ser el dorsal del participante
      * @return devuelve el objeto encontrado o null
      */
-    public Participation searchParticipant(Integer dorsal){
-        Participation result=null;
-        if(participations!=null && participations.get(dorsal)!=null){
-            result=participations.get(dorsal);
+    public Participation searchParticipant(Integer dorsal) {
+        Participation result = null;
+        if (participations != null && participations.get(dorsal) != null) {
+            result = participations.get(dorsal);
         }
         return result;
     }
