1fca34ed52eda9e7d33f5b5ad65e2f406f0aca3a
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-19 15:11:55 +0100

    Delete src/main/java/iesFranciscodelosRios directory

--- src/main/java/iesFranciscodelosRios/interfaces/iParticipation.java
+++
@@ -1,9 +0,0 @@
-package iesFranciscodelosRios.interfaces;
-
-public interface iParticipation {
-    Integer getDorsal();
-    int points=0;
-    int getPoints();
-    void setPoints(int points);
-    String toString();
-}
