6a4a2cbd185de31ec1d00769ebc04128c21b3dbd
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-03-22 09:31:46 +0100

    Delete GymnasticCompetition directory

--- GymnasticCompetition/src/main/java/iesFranciscodelosRios/interfaces/iRepoGymnast.java
+++
@@ -1,7 +0,0 @@
-package iesFranciscodelosRios.interfaces;
-import iesFranciscodelosRios.model.Gymnast;
-public interface iRepoGymnast {
-	boolean addGymnast(Gymnast gym);
-	boolean deleteGymnast(String DNI);
-	Gymnast showGymnast(String dni);
-}
